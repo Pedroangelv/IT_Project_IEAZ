02559c3a0772a153351fa041999a6b8dd84bb828
Pedroangelv <grijalbaangel393@gmail.com>
2025-03-10 23:24:54 -0500

    basic practice

--- Games/Trivia/script.js
+++ Games/Trivia/script.js
@@ -1,3 +1,27 @@
-function generarPregunta() {
-  //
+éscogerPregunta(0);
+
+function escogerPregunta(n) {
+  let base_pregunta = readText("base-preguntas.json");
+  let interprete_bp = JSON.parse(base_preguntas);
+  pregunta = interprete_bp[n];
+  select_id("categoria").innerHTML = pregunta.categoria;
+  select_id("pregunta").innerHTML = pregunta.pregunta;
+}
+
+function select_id(id) {
+  return document.getElementById;
+}
+function style(id) {
+  return select_id(id).style;
+}
+
+function readText(ruta_local) {
+  var texto = null;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("GET", ruta_local, false);
+  xmlhttp.send();
+  if ((xmlhttp.status = 200)) {
+    texto = xmlhttp.responseText;
+  }
+  return texto;
 }
