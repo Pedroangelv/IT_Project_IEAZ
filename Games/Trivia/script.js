a8dc4e95e0a626c93056056c65a3a9fb8b32bcdf
mmell0988 <loaizammello@gmail.com>
2025-03-11 22:10:25 -0300

    trivia 11

--- Games/Trivia/script.js
+++ Games/Trivia/script.js
@@ -1,18 +1,24 @@
-éscogerPregunta(0);
+escogerPregunta(0)
 
 function escogerPregunta(n) {
-  let base_pregunta = readText("base-preguntas.json");
-  let interprete_bp = JSON.parse(base_preguntas);
-  pregunta = interprete_bp[n];
-  select_id("categoria").innerHTML = pregunta.categoria;
-  select_id("pregunta").innerHTML = pregunta.pregunta;
+  let base_preguntas = readText("base-preguntas.json")
+  let interprete_bp = JSON.parse(base_preguntas)
+  pregunta = interprete_bp[n]
+  select_id("categoria").innerHTML = pregunta.categoria
+  select_id("pregunta").innerHTML = pregunta.pregunta
+  select_id("imagen").setAttribute("src" ,pregunta.imagen)
+  style("imagen").objectfit = pregunta.objectfit;
+  select_id("btn1").innerHTML = pregunta.respuesta
+  select_id("btn2").innerHTML = pregunta.incorrecta1
+  select_id("btn3").innerHTML = pregunta.incorrecta2
+  select_id("btn4").innerHTML = pregunta.incorrecta3
 }
 
 function select_id(id) {
-  return document.getElementById;
+  return document.getElementById(id)
 }
 function style(id) {
-  return select_id(id).style;
+  return select_id(id).style
 }
 
 function readText(ruta_local) {
