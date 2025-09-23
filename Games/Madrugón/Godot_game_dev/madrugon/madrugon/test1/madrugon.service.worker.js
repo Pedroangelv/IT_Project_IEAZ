00cee7b3087d4fa77bee899bbb555c25d57a0629
Pedroangelv <grijalbaangel393@gmail.com>
2025-09-23 11:41:48 -0500

    Madrugon Demo 2.4

--- Games/Madrugón/Godot_game_dev/madrugon/madrugon/test1/madrugon.service.worker.js
+++ Games/Madrugón/Godot_game_dev/madrugon/madrugon/test1/madrugon.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1758586647|17558042';
+const CACHE_VERSION = '1758645666|157583441';
 /** @type {string} */
 const CACHE_PREFIX = 'madrugon-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
